75559630b4e1e3d12b7f21fc33dd980288aa02ee
BT_4N2_17 <s@>
2024-03-21 14:02:37 -0400

    initial commitment

--- src/Deck.java
+++ src/Deck.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class Deck {
     private ArrayList<Card> deck;
 
+    public Deck() {
+        deck = new ArrayList<Card>();
+    }
+
     public ArrayList<Card> buildDeck() {
         String[] suits = {"clubs", "diamonds", "hearts", "spades"};
         String[] values = {"02", "03", "04", "05", "06", "07", "08", "09", "10", "A", "J", "K", "Q"};
